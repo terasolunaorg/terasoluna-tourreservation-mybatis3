terasolunaorg/terasoluna-tourreservation

Improve overall. terasolunaorg/terasoluna-tourreservation#243
* URL designs
* format in JSP
* button implementation in JSP
* Spring Security configurations and jsp implementations
* Move package
* etc ...

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourController.java
@@ -15,20 +15,16 @@
  */
 package org.terasoluna.tourreservation.app.reservetour;
 
-import javax.inject.Inject;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.web.token.transaction.TransactionTokenCheck;
@@ -36,14 +32,15 @@ import org.terasoluna.gfw.web.token.transaction.TransactionTokenType;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourOutput;
 import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUserDetails;
 
+import javax.inject.Inject;
+
 /**
  * Handle request of tour searching.
  */
 @Slf4j
 @Controller
-@RequestMapping(value = "reservetour")
-@SessionAttributes(types = ReserveTourForm.class)
-@TransactionTokenCheck(value = "reservetour")
+@RequestMapping(value = "tours")
+@TransactionTokenCheck(value = "tours")
 public class ReserveTourController {
 
     @Inject
@@ -64,14 +61,12 @@ public class ReserveTourController {
      * @param model
      * @return
      */
-    @RequestMapping(value = "read", method = RequestMethod.GET)
-    public String reserveForm(@AuthenticationPrincipal ReservationUserDetails userDetails, ReserveTourForm form, Model model,
-            SessionStatus status) {
-        log.debug("retieve tour {}", form.getTourCode());
+    @RequestMapping(value = {"{tourCode}", "{tourCode}/reserve"}, method = RequestMethod.GET, params = "form")
+    public String reserveForm(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            ReserveTourForm form, Model model) {
+        log.debug("retrieve tour {}", tourCode);
 
-        status.setComplete();
-
-        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
+        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, tourCode, form);
 
         model.addAttribute("output", output);
 
@@ -85,18 +80,18 @@ public class ReserveTourController {
      * @param model
      * @return
      */
-    @TransactionTokenCheck(value = "reserve", type = TransactionTokenType.BEGIN)
-    @RequestMapping(value = "reserve", method = RequestMethod.POST, params = "confirm")
-    public String confirm(@AuthenticationPrincipal ReservationUserDetails userDetails, @Validated ReserveTourForm form,
-            BindingResult bindingResult, Model model) {
+    @TransactionTokenCheck(value="reserve", type = TransactionTokenType.BEGIN)
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "confirm")
+    public String confirm(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            @Validated ReserveTourForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
         log.debug(
                 "confirm the reservation details for the following tour {}",
-                form.getTourCode());
+                tourCode);
 
-        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
+        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, tourCode, form);
         model.addAttribute("output", output);
 
         return "reservetour/reserveConfirm";
@@ -109,44 +104,41 @@ public class ReserveTourController {
      * @param redirectAttr
      * @return
      */
-    @TransactionTokenCheck(value = "reserve", type = TransactionTokenType.IN)
-    @RequestMapping(value = "reserve", method = RequestMethod.POST)
-    public String reserve(@AuthenticationPrincipal ReservationUserDetails userDetails, @Validated ReserveTourForm form,
-            BindingResult bindingResult, RedirectAttributes redirectAttr,
-            Model model) {
-        log.debug("reserve tour {}", form.getTourCode());
+    @TransactionTokenCheck(value="reserve", type = TransactionTokenType.IN)
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST)
+    public String reserve(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            @Validated ReserveTourForm form, BindingResult bindingResult, Model model, RedirectAttributes redirectAttr) {
+        log.debug("reserve tour {}", tourCode);
 
         if (bindingResult.hasErrors()) {
-            TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
-            model.addAttribute("output", output);
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
 
         try {
-            ReserveTourOutput output = reserveTourHelper.reserve(userDetails, form);
+            ReserveTourOutput output = reserveTourHelper.reserve(userDetails, tourCode, form);
             redirectAttr.addFlashAttribute("output", output);
         } catch (BusinessException e) {
-            TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
-            model.addAttribute("output", output);
             model.addAttribute(e.getResultMessages());
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
 
-        return "redirect:/reservetour/reserve?complete";
+        redirectAttr.addAttribute("tourCode", tourCode);
+        return "redirect:/tours/{tourCode}/reserve?complete";
     }
 
     /**
      * redirects to the reservation completion page
      * @return
      */
-    @RequestMapping(value = "reserve", method = RequestMethod.GET, params = "complete")
-    public String reserveComplete(SessionStatus status) {
-        status.setComplete();
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.GET, params = "complete")
+    public String reserveComplete() {
         return "reservetour/reserveComplete";
     }
 
-    @RequestMapping(value = "read", method = RequestMethod.POST, params = "redo")
-    public String reserveRedo() {
-        return "redirect:/reservetour/read";
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "redo")
+    public String reserveRedo(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            ReserveTourForm form,Model model) {
+        return reserveForm(userDetails, tourCode, form, model);
     }
+
 }

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourHelper.java
@@ -26,7 +26,7 @@ import org.terasoluna.tourreservation.domain.service.reserve.ReserveService;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourInput;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourOutput;
 import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateOutput;
-import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateSharedSerivce;
+import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateSharedService;
 import org.terasoluna.tourreservation.domain.service.tourinfo.TourInfoSharedService;
 import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUserDetails;
 
@@ -34,7 +34,7 @@ import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUser
 public class ReserveTourHelper {
 
 	@Inject
-	PriceCalculateSharedSerivce priceCalculateService;
+	PriceCalculateSharedService priceCalculateService;
 
 	@Inject
 	ReserveService reserveService;
@@ -43,7 +43,7 @@ public class ReserveTourHelper {
 	TourInfoSharedService tourInfoSharedService;
 
 	@Inject
-	Mapper dozerBeanMapper;
+	Mapper beanMapper;
 
 	/**
 	 * Fetches detailed information of a particular tour (associated entities
@@ -52,13 +52,14 @@ public class ReserveTourHelper {
 	 * @param form
 	 * @return
 	 */
-	public TourDetailOutput findTourDetail(ReservationUserDetails userDetails, ReserveTourForm form) {
-		TourDetailOutput output = new TourDetailOutput();
-		TourInfo tourInfo = tourInfoSharedService.findOne(form.getTourCode());
+	public TourDetailOutput findTourDetail(ReservationUserDetails userDetails, String tourCode, ReserveTourForm form) {
+		TourInfo tourInfo = tourInfoSharedService.findOne(tourCode);
 
 		PriceCalculateOutput priceCalculateOutput = priceCalculateService
 				.calculatePrice(tourInfo.getBasePrice(), form.getAdultCount(),
 						form.getChildCount());
+
+		TourDetailOutput output = new TourDetailOutput();
 		output.setTourInfo(tourInfo);
 		output.setPriceCalculateOutput(priceCalculateOutput);
 		
@@ -70,16 +71,18 @@ public class ReserveTourHelper {
 
 	/**
 	 * makes a reservation
-	 * 
-	 * @param tourDetailForm
+	 *
+	 * @param userDetails
+	 * @param tourReserveForm
 	 * @return
 	 * @throws BusinessException
 	 */
-	public ReserveTourOutput reserve(ReservationUserDetails userDetails, ReserveTourForm tourReserveForm)
+	public ReserveTourOutput reserve(ReservationUserDetails userDetails, String tourCode, ReserveTourForm tourReserveForm)
 			throws BusinessException {
 
-		ReserveTourInput input = dozerBeanMapper.map(tourReserveForm,
+		ReserveTourInput input = beanMapper.map(tourReserveForm,
 				ReserveTourInput.class);
+		input.setTourCode(tourCode);
 
 		Customer customer = userDetails.getCustomer();
 		input.setCustomer(customer);

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/searchtour/SearchTourController.java
@@ -48,7 +48,7 @@ import org.terasoluna.tourreservation.domain.service.tourinfo.TourInfoService;
  */
 @Slf4j
 @Controller
-@RequestMapping(value = "searchtour")
+@RequestMapping(value = "tours")
 @SessionAttributes(types = SearchTourForm.class)
 public class SearchTourController {
 
@@ -71,7 +71,6 @@ public class SearchTourController {
 
     /**
      * pre-initialization of form backed bean
-     * @param model
      * @return
      */
     @ModelAttribute
@@ -91,7 +90,7 @@ public class SearchTourController {
      * shows the search input view
      * @return search input view
      */
-    @RequestMapping(value = "search", method = RequestMethod.GET, params = "form")
+    @RequestMapping(method = RequestMethod.GET, params = "form")
     public String searchForm(SessionStatus status) {
         status.setComplete();
         return "searchtour/searchForm";
@@ -100,13 +99,13 @@ public class SearchTourController {
     /**
      * Searches the tours based on user input. Search result is shown in the form of a List of TourInfo domain objects User
      * input is mapped to form backed bean SearchTourForm
-     * @param form
+     * @param searchTourForm
      * @param result
      * @param model
      * @param pageable
      * @return
      */
-    @RequestMapping(value = "search", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String search(@Validated SearchTourForm searchTourForm,
             BindingResult result, Model model,
             @PageableDefault Pageable pageable) {
@@ -115,7 +114,7 @@ public class SearchTourController {
         }
 
         if (log.isDebugEnabled()) {
-            log.info("pageable={}", pageable);
+            log.debug("pageable={}", pageable);
         }
         
         TourInfoSearchCriteria criteria = beanMapper.map(searchTourForm, TourInfoSearchCriteria.class);

terasolunaorg/terasoluna-tourreservation-mybatis3

(cherry picked from commit 7978e2f56a0ab82de7fa6d1bf061a729fb853bee)
Conflicts:
	terasoluna-tourreservation-domain/src/test/java/org/terasoluna/tourreservation/domain/service/reserve/ReserveServiceImplTest.java
	terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/managereservation/ManageReservationController.java
	terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourHelper.java
	terasoluna-tourreservation-web/src/main/resources/META-INF/spring/applicationContext.xml
  Those conflicts was resolved by manually.
(cherry picked from commit eb6519cbf886d7a229a19029c9bc9150fe1b335b)
	terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourController.java
	terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/searchtour/SearchTourController.java

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourController.java
@@ -15,8 +15,6 @@
  */
 package org.terasoluna.tourreservation.app.reservetour;
 
-import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
@@ -25,10 +23,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.web.token.transaction.TransactionTokenCheck;
@@ -36,13 +33,14 @@ import org.terasoluna.gfw.web.token.transaction.TransactionTokenType;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourOutput;
 import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUserDetails;
 
+import javax.inject.Inject;
+
 /**
  * Handle request of tour searching.
  */
 @Controller
-@RequestMapping(value = "reservetour")
-@SessionAttributes(types = ReserveTourForm.class)
-@TransactionTokenCheck(value = "reservetour")
+@RequestMapping(value = "tours")
+@TransactionTokenCheck(value = "tours")
 public class ReserveTourController {
 
     private static final Logger logger = LoggerFactory
@@ -66,14 +64,12 @@ public class ReserveTourController {
      * @param model
      * @return
      */
-    @RequestMapping(value = "read", method = RequestMethod.GET)
-    public String reserveForm(@AuthenticationPrincipal ReservationUserDetails userDetails, ReserveTourForm form, Model model,
-            SessionStatus status) {
-        logger.debug("retieve tour {}", form.getTourCode());
+    @RequestMapping(value = {"{tourCode}", "{tourCode}/reserve"}, method = RequestMethod.GET, params = "form")
+    public String reserveForm(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            ReserveTourForm form, Model model) {
+        logger.debug("retrieve tour {}", tourCode);
 
-        status.setComplete();
-
-        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
+        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, tourCode, form);
 
         model.addAttribute("output", output);
 
@@ -87,18 +83,18 @@ public class ReserveTourController {
      * @param model
      * @return
      */
-    @TransactionTokenCheck(value = "reserve", type = TransactionTokenType.BEGIN)
-    @RequestMapping(value = "reserve", method = RequestMethod.POST, params = "confirm")
-    public String confirm(@AuthenticationPrincipal ReservationUserDetails userDetails, @Validated ReserveTourForm form,
-            BindingResult bindingResult, Model model) {
+    @TransactionTokenCheck(value="reserve", type = TransactionTokenType.BEGIN)
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "confirm")
+    public String confirm(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            @Validated ReserveTourForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
         logger.debug(
                 "confirm the reservation details for the following tour {}",
-                form.getTourCode());
+                tourCode);
 
-        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
+        TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, tourCode, form);
         model.addAttribute("output", output);
 
         return "reservetour/reserveConfirm";
@@ -111,44 +107,41 @@ public class ReserveTourController {
      * @param redirectAttr
      * @return
      */
-    @TransactionTokenCheck(value = "reserve", type = TransactionTokenType.IN)
-    @RequestMapping(value = "reserve", method = RequestMethod.POST)
-    public String reserve(@AuthenticationPrincipal ReservationUserDetails userDetails, @Validated ReserveTourForm form,
-            BindingResult bindingResult, RedirectAttributes redirectAttr,
-            Model model) {
-        logger.debug("reserve tour {}", form.getTourCode());
+    @TransactionTokenCheck(value="reserve", type = TransactionTokenType.IN)
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST)
+    public String reserve(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            @Validated ReserveTourForm form, BindingResult bindingResult, Model model, RedirectAttributes redirectAttr) {
+        logger.debug("reserve tour {}", tourCode);
 
         if (bindingResult.hasErrors()) {
-            TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
-            model.addAttribute("output", output);
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
 
         try {
-            ReserveTourOutput output = reserveTourHelper.reserve(userDetails, form);
+            ReserveTourOutput output = reserveTourHelper.reserve(userDetails, tourCode, form);
             redirectAttr.addFlashAttribute("output", output);
         } catch (BusinessException e) {
-            TourDetailOutput output = reserveTourHelper.findTourDetail(userDetails, form);
-            model.addAttribute("output", output);
             model.addAttribute(e.getResultMessages());
-            return "reservetour/reserveForm";
+            return reserveForm(userDetails, tourCode, form, model);
         }
 
-        return "redirect:/reservetour/reserve?complete";
+        redirectAttr.addAttribute("tourCode", tourCode);
+        return "redirect:/tours/{tourCode}/reserve?complete";
     }
 
     /**
      * redirects to the reservation completion page
      * @return
      */
-    @RequestMapping(value = "reserve", method = RequestMethod.GET, params = "complete")
-    public String reserveComplete(SessionStatus status) {
-        status.setComplete();
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.GET, params = "complete")
+    public String reserveComplete() {
         return "reservetour/reserveComplete";
     }
 
-    @RequestMapping(value = "read", method = RequestMethod.POST, params = "redo")
-    public String reserveRedo() {
-        return "redirect:/reservetour/read";
+    @RequestMapping(value = "{tourCode}/reserve", method = RequestMethod.POST, params = "redo")
+    public String reserveRedo(@AuthenticationPrincipal ReservationUserDetails userDetails, @PathVariable("tourCode") String tourCode,
+            ReserveTourForm form,Model model) {
+        return reserveForm(userDetails, tourCode, form, model);
     }
+
 }

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/reservetour/ReserveTourHelper.java
@@ -26,7 +26,7 @@ import org.terasoluna.tourreservation.domain.service.reserve.ReserveService;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourInput;
 import org.terasoluna.tourreservation.domain.service.reserve.ReserveTourOutput;
 import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateOutput;
-import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateSharedSerivce;
+import org.terasoluna.tourreservation.domain.service.tourinfo.PriceCalculateSharedService;
 import org.terasoluna.tourreservation.domain.service.tourinfo.TourInfoSharedService;
 import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUserDetails;
 
@@ -34,7 +34,7 @@ import org.terasoluna.tourreservation.domain.service.userdetails.ReservationUser
 public class ReserveTourHelper {
 
 	@Inject
-	PriceCalculateSharedSerivce priceCalculateService;
+	PriceCalculateSharedService priceCalculateService;
 
 	@Inject
 	ReserveService reserveService;
@@ -43,7 +43,7 @@ public class ReserveTourHelper {
 	TourInfoSharedService tourInfoSharedService;
 
 	@Inject
-	Mapper dozerBeanMapper;
+	Mapper beanMapper;
 
 	/**
 	 * Fetches detailed information of a particular tour (associated entities
@@ -52,13 +52,14 @@ public class ReserveTourHelper {
 	 * @param form
 	 * @return
 	 */
-	public TourDetailOutput findTourDetail(ReservationUserDetails userDetails, ReserveTourForm form) {
-		TourDetailOutput output = new TourDetailOutput();
-		TourInfo tourInfo = tourInfoSharedService.findOneWithDetails(form.getTourCode());
+	public TourDetailOutput findTourDetail(ReservationUserDetails userDetails, String tourCode, ReserveTourForm form) {
+		TourInfo tourInfo = tourInfoSharedService.findOneWithDetails(tourCode);
 
 		PriceCalculateOutput priceCalculateOutput = priceCalculateService
 				.calculatePrice(tourInfo.getBasePrice(), form.getAdultCount(),
 						form.getChildCount());
+
+		TourDetailOutput output = new TourDetailOutput();
 		output.setTourInfo(tourInfo);
 		output.setPriceCalculateOutput(priceCalculateOutput);
 		
@@ -70,16 +71,18 @@ public class ReserveTourHelper {
 
 	/**
 	 * makes a reservation
-	 * 
-	 * @param tourDetailForm
+	 *
+	 * @param userDetails
+	 * @param tourReserveForm
 	 * @return
 	 * @throws BusinessException
 	 */
-	public ReserveTourOutput reserve(ReservationUserDetails userDetails, ReserveTourForm tourReserveForm)
+	public ReserveTourOutput reserve(ReservationUserDetails userDetails, String tourCode, ReserveTourForm tourReserveForm)
 			throws BusinessException {
 
-		ReserveTourInput input = dozerBeanMapper.map(tourReserveForm,
+		ReserveTourInput input = beanMapper.map(tourReserveForm,
 				ReserveTourInput.class);
+		input.setTourCode(tourCode);
 
 		Customer customer = userDetails.getCustomer();
 		input.setCustomer(customer);

terasoluna-tourreservation-web/src/main/java/org/terasoluna/tourreservation/app/searchtour/SearchTourController.java
@@ -47,7 +47,7 @@ import org.terasoluna.tourreservation.domain.service.tourinfo.TourInfoService;
  * Handle request of tour searching.
  */
 @Controller
-@RequestMapping(value = "searchtour")
+@RequestMapping(value = "tours")
 @SessionAttributes(types = SearchTourForm.class)
 public class SearchTourController {
 
@@ -73,7 +73,6 @@ public class SearchTourController {
 
     /**
      * pre-initialization of form backed bean
-     * @param model
      * @return
      */
     @ModelAttribute
@@ -93,7 +92,7 @@ public class SearchTourController {
      * shows the search input view
      * @return search input view
      */
-    @RequestMapping(value = "search", method = RequestMethod.GET, params = "form")
+    @RequestMapping(method = RequestMethod.GET, params = "form")
     public String searchForm(SessionStatus status) {
         status.setComplete();
         return "searchtour/searchForm";
@@ -102,13 +101,13 @@ public class SearchTourController {
     /**
      * Searches the tours based on user input. Search result is shown in the form of a List of TourInfo domain objects User
      * input is mapped to form backed bean SearchTourForm
-     * @param form
+     * @param searchTourForm
      * @param result
      * @param model
      * @param pageable
      * @return
      */
-    @RequestMapping(value = "search", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String search(@Validated SearchTourForm searchTourForm,
             BindingResult result, Model model,
             @PageableDefault Pageable pageable) {
@@ -117,7 +116,7 @@ public class SearchTourController {
         }
 
         if (logger.isDebugEnabled()) {
-            logger.info("pageable={}", pageable);
+            logger.debug("pageable={}", pageable);
         }
         
         TourInfoSearchCriteria criteria = beanMapper.map(searchTourForm, TourInfoSearchCriteria.class);